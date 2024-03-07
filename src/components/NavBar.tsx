a23891c51172a6b013ab9a6863c76e9348718b5c
RehanRanav <155937908+RehanRanav@users.noreply.github.com>
2024-03-07 14:05:03 +0530

    Add carousel in navbar
    Add carousel in navbar and customize and animate it

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Card, Carousel } from "flowbite-react";
 import logo from "../assets/logo-dreamsoft.png";
+import NavbarImg1 from "../assets/navbar-img-1.jpg";
+import NavbarImg2 from "../assets/navbar-img-2.jpg";
+import NavbarImg3 from "../assets/navbar-img-3.jpg";
+import NavbarImg4 from "../assets/navbar-img-4.jpg";
+import NavbarImg5 from "../assets/navbar-img-5.jpg";
+import NavbarImg6 from "../assets/navbar-img-6.jpg";
 import { IoSearchOutline } from "react-icons/io5";
 import { PiShareNetworkLight } from "react-icons/pi";
 import { MdOutlineCancel } from "react-icons/md";
-import { useState } from "react";
-import { Card } from "flowbite-react";
+import { AiOutlineZoomIn } from "react-icons/ai";
 
 const NavBar = () => {
   const [enableSearch, setEnableSearch] = useState(false);
@@ -33,7 +40,7 @@ const NavBar = () => {
           </Link>
           <Link to="/" className="group relative">
             <div>Gallery</div> <HoverlineEffect />
-            <Card className="rounded-none w-60 px-4 absolute top-16 whitespace-nowrap opacity-0 group-hover:opacity-100 hover:block group-hover:transform group-hover:top-14 transition-all duration-300 ease-in">
+            <Card className="rounded-none w-60 px-4 absolute top-16 whitespace-nowrap opacity-0 group-hover:opacity-100 hidden group-hover:block hover:block group-hover:transform group-hover:top-14 transition-all duration-300 ease-in">
               <ul className="flex flex-col gap-3">
                 <li className="hover:border-l-4 hover:border-[#756aee] hover:pl-4 transition-all duration-300 ease-in-out">
                   Full width Gallery
@@ -56,7 +63,7 @@ const NavBar = () => {
           </Link>
           <Link to="/" className="group relative">
             <div>Blog</div> <HoverlineEffect />
-            <Card className="rounded-none w-60 px-4 absolute top-16 whitespace-nowrap opacity-0 group-hover:opacity-100 hover:block group-hover:transform group-hover:top-14 transition-all duration-300 ease-in">
+            <Card className="rounded-none w-60 px-4 absolute top-16 whitespace-nowrap opacity-0 group-hover:opacity-100 hidden group-hover:block hover:block group-hover:transform group-hover:top-14 transition-all duration-300 ease-in">
               <ul className="flex flex-col gap-3">
                 {[
                   ["Classic Blog"],
@@ -75,11 +82,12 @@ const NavBar = () => {
           <Link to="/" className="group">
             <div>Contact Us</div> <HoverlineEffect />
           </Link>
-          <Link to="/" className="group relative">
+          <Link to="/" className="group relative flex items-center">
             <div>Pages</div> <HoverlineEffect />
-            <Card className="rounded-none w-screen px-4 grid grid-cols-3 absolute top-16 -right-[150px] whitespace-nowrap opacity-0 group-hover:opacity-100 hover:block group-hover:transform group-hover:top-14 transition-all duration-300 ease-in">
-              <div className="border-r-2">
-                <div>Elements</div>
+            <Card className="rounded-none w-screen px-4 absolute -right-[150px] whitespace-nowrap hidden group-hover:block hover:block group-hover:transform group-hover:top-14 transition-display transition-all duration-1000 ease-in">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="border-r-2 font-normal">
+                  <div>Elements</div>
                   <ul className="grid grid-cols-2 grid-rows-6">
                     {[
                       ["Typography"],
@@ -97,11 +105,11 @@ const NavBar = () => {
                         {item}
                       </li>
                     ))}
-                </ul>                  
-              </div>
-              
-              <div className="border-r-2">
-                <div>Additional Pages</div>
+                  </ul>
+                </div>
+
+                <div className="border-r-2 font-normal">
+                  <div>Additional Pages</div>
                   <ul className="grid grid-cols-2 grid-rows-6">
                     {[
                       ["Typography"],
@@ -119,7 +127,117 @@ const NavBar = () => {
                         {item}
                       </li>
                     ))}
-                </ul>                  
+                  </ul>
+                </div>
+
+                <div>
+                  <div>Recent Projects</div>
+                  <div className="px-4 h-56 w-96 cursor-pointer">
+                    <Carousel
+                      leftControl={` `}
+                      rightControl={` `}
+                      indicators={true}
+                      className="cursor-pointer"
+                    >
+                      <div className="grid grid-cols-2 gap-2">
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg1})`,
+                            height: "140px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #1</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg2})`,
+                            height: "140px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #2</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="grid grid-cols-2 gap-2">
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg3})`,
+                            height: "140px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #3</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg4})`,
+                            height: "140px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #4</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="grid grid-cols-2 gap-2">
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg5})`,
+                            height: "150px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #5</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          className={`h-fit w-fit bg-no-repeat bg-cover bg-center transition duration-300 ease-in-out relative cursor-pointer`}
+                          style={{
+                            backgroundImage: `url(${NavbarImg6})`,
+                            height: "150px",
+                            width: "142px",
+                          }}
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-0 hover:opacity-50 hover:scale-105 transition-all duration-300 ease-in-out">
+                            <div className="m-2 ml-3 border h-[124px] border-white flex flex-col gap-2 justify-center items-center text-white">
+                              <AiOutlineZoomIn size={30} />
+                              <div className="hover:border-b-2 border-b-white">Project #6</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Carousel>
+                  </div>
+                </div>
               </div>
             </Card>
           </Link>
@@ -128,7 +246,7 @@ const NavBar = () => {
         <div className="relative w-full flex-2">
           <input
             type="text"
-            className={`block w-full transition-all duration-1000 ease-in ${
+            className={`block w-full transition-all duration-1000 ease-linear ${
               enableSearch ? `scale-x-100` : ``
             } z-10 p-4 ps-10 text-sm text-gray-900 border-none shadow-md border-gray-300 rounded-full bg-gray-50 outline-none focus:border-none`}
             placeholder="Search..."
